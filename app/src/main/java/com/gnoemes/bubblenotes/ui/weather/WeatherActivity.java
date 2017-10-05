2e3c2ea4d723861d9590860e5dd7c1b4ecc1ead6
kenji47 <fw190a2@gmail.com>
2017-10-05 09:56:50 +0300

    Clean up code

---
+++ app/src/main/java/com/gnoemes/bubblenotes/ui/weather/WeatherActivity.java
@@ -0,0 +1,10 @@
+package com.gnoemes.bubblenotes.ui.weather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kenji1947 on 05.10.2017.
+ */
+
+public class WeatherActivity extends AppCompatActivity {
+}
